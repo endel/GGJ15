b578a483c7fee8e6ec8e52ed424f946578e3958c
Vitaliano <vitaliano.neto@gmail.com>
2015-01-24 17:20:37 -0200

    Cleaned the code

--- public/src/main.js
+++ public/src/main.js
@@ -38,8 +38,6 @@ var gridState = new Array(gridWidth);
 
 var allBoxes = [];
 var graphics = null;
-var boxesGroup = null;
-var boxesCollision = null
 
 function preload() {
   for (var i = gridWidth - 1; i >= 0; i--) {
@@ -48,29 +46,10 @@ function preload() {
       gridState[i][j] = 0;
     }
   }
-  //  You can fill the preloader with as many assets as your game requires
-
-  //  Here we are loading an image. The first parameter is the unique
-  //  string by which we'll identify the image later in our code.
-
-  //  The second parameter is the URL of the image (relative)
   game.load.image('box', 'assets/images/elementos_01.png');
 }
 
 function create() {
-
-  //  This creates a simple sprite that is using our loaded image and
-  //  displays it on-screen
-  //game.add.sprite(0, 0, 'kitten');
-
-  // game.physics.startSystem(Phaser.Physics.P2JS);
-  // game.physics.p2.setImpactEvents(true);
-  // game.physics.p2.restitution = 0.8;
-  // game.physics.p2.applyGravity = true;
-  // game.physics.p2.gravity.y = 250;
-  // boxesCollision = game.physics.p2.createCollisionGroup();
-  // game.physics.p2.updateBoundsCollisionGroup();
-
   graphics = game.add.graphics(0, 0);
   game.input.onDown.add(function() {
     socket.emit('add_block', {
@@ -95,32 +74,12 @@ function createBox(data) {
   var col = Math.floor(data.y / gridSizePx);
   var posx = row * gridSizePx;
   var posy = col * gridSizePx;
-  //console.log("x: ", game.input.x, " posx:", posx, " | y:", game.input.y, " posy:", posy);
   var box = game.add.sprite(posx, posy, 'box');
   box.width = gridSizePx;
   box.height = gridSizePx;
   gridState[row][col] = box;
   box.col = col;
   box.row = row;
-  // var box = boxesGroup.create(posx, posy, 'box');
-  // box.body.setRectangleFromSprite(box);
-  // box.body.fixedRotation = true;
-
-  // box.body.setCollisionGroup(boxesCollision);
-  // box.body.collides(boxesCollision);
-  /*box.body.collideWorldBounds = true;
-  box.body.allowGravity = true;
-  box.body.bounce.y = 0.1;
-  box.body.checkCollision = true;
-  //box.body.checkCollision.up = true;
-  //box.body.immovable = true;
-
-  for (var i = allBoxes.length - 1; i >= 0; i--) {
-    if(allBoxes[i].x == box.x) {
-      console.log("found");
-      game.physics.arcade.collide(box, allBoxes[i]);
-    }
-  }*/
   allBoxes.push(box);
   console.log("createBox");
 }
