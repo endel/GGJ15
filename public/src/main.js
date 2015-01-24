a16d4b860ecd12ade3acb38e95ef34f91c19b5db
Vitaliano <vitaliano.neto@gmail.com>
2015-01-24 14:38:11 -0200

    Blocks falling with gravity and spawning with mouse click

--- public/src/main.js
+++ public/src/main.js
@@ -2,8 +2,18 @@ var realtime = require('./realtime');
 
 var game = new Phaser.Game(800, 600, Phaser.WEBGL, 'phaser-example', {
   preload: preload,
-  create: create
+  create: create,
+  update: update,
+  render: render
 });
+
+
+var gridWidth = 16;
+var gridHeight = 15;
+var gridSizePx = 50;
+
+var allBoxes = [];
+var graphics = null;
 
 function preload() {
   //  You can fill the preloader with as many assets as your game requires
@@ -12,12 +22,49 @@ function preload() {
   //  string by which we'll identify the image later in our code.
 
   //  The second parameter is the URL of the image (relative)
-  game.load.image('kitten', 'assets/images/kitten.png');
+  game.load.image('box', 'assets/images/elementos_01.png');
 }
 
 function create() {
 
   //  This creates a simple sprite that is using our loaded image and
   //  displays it on-screen
-  game.add.sprite(0, 0, 'kitten');
+  //game.add.sprite(0, 0, 'kitten');
+
+  game.physics.startSystem(Phaser.Physics.ARCADE);
+  game.physics.arcade.gravity.y = 250;
+
+  graphics = game.add.graphics(0, 0);
+  game.input.onDown.add(createBox, this);
+}
+
+function createBox() {
+  var posx = game.input.x;
+  var posy = game.input.y;
+  //var box = new Phaser.Rectangle(posx, posy, gridSizePx, gridSizePx);
+  var box = game.add.sprite(posx, posy, 'box');
+  box.width = gridSizePx;
+  box.height = gridSizePx;
+  game.physics.enable(box, Phaser.Physics.ARCADE);
+  box.body.collideWorldBounds = true;
+  box.body.allowGravity = true;
+  box.body.bounce.y = 0.1;
+  allBoxes.push(box);
+  console.log("createBox");
+}
+
+function update () {
+
+}
+
+function render() {
+  graphics.lineStyle(2, 0xFFFFFF);
+  for (var i = 0; i <= gridWidth; i++) {
+    graphics.moveTo(i*gridSizePx, 0);
+    graphics.lineTo(i*gridSizePx, gridSizePx*gridHeight);
+  }
+  for (var j = 0; j <= gridHeight; j++) {
+    graphics.moveTo(0, j*gridSizePx);
+    graphics.lineTo(gridSizePx*gridWidth, j*gridSizePx);
+  }
 }
--- public/src/realtime.js
+++ public/src/realtime.js
@@ -2,7 +2,7 @@ window.io = require('socket.io-client');
 var socket = io.connect();
 
 socket.on('connect', function(){
-  socket.emit('adduser', prompt("What's your name: "));
+  //socket.emit('adduser', prompt("What's your name: "));
 });
 
 socket.on('updatechat', function (username, data) {
